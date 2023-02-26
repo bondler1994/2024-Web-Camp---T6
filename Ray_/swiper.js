93db1702a6cb61bb5d1cae178228bdb45e8fe3b8
Ray <silent0359@gmail.com>
2023-02-26 15:28:54 +0800

    測試body背景的方法、用js調整swiper的rwd、捨棄bootstrap的grid system改用grid、改變引入scss檔的方式

--- Ray_/swiper.js
+++ Ray_/swiper.js
@@ -1,20 +1,41 @@
-var swiper = new Swiper('.swiper', {
-  slidesPerView: 4,
-  direction: getDirection(),
+var swiper = new Swiper(".swiper", {
+
+  slidesPerView: 'auto',
+
+  breakpoints: { 
+    375: {  
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    576: {  
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    768: {  
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    1200: {  
+      slidesPerView: 4,
+      spaceBetween: 30
+    }
+  },
+
+  direction: "horizontal",
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   on: {
     resize: function () {
-      swiper.changeDirection(getDirection());
+      swiper.changeDirection("horizontal");
     },
   },
 });
 
-function getDirection() {
-  var windowWidth = window.innerWidth;
-  var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+// function getDirection() {
+//   var windowWidth = window.innerWidth;
+//   var direction = window.innerWidth <= 760 ? "vertical" : "horizontal";
 
-  return direction;
-}
+//   return direction;
+// }
