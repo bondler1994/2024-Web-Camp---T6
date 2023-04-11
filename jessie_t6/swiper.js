221dcc25e9e25622573f369a1ad2fb46956639c1
“Jessie <eee222j@mail.com>
2023-04-11 20:18:09 +0800

    t6_v0

--- jessie_t6/swiper.js
+++ jessie_t6/swiper.js
@@ -1,7 +1,9 @@
 
 var swiper = new Swiper(".swiper", {
   slidesPerView: 4,
-  direction: getDirection(),
+  spaceBetween: 30,
+  slidesPerGroup: 4,
+ 
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -11,11 +13,24 @@ var swiper = new Swiper(".swiper", {
       swiper.changeDirection(getDirection());
     },
   },
+  breakpoints: {
+    320: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+        slidesPerGroup: 2,
+    },
+    576: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        slidesPerGroup: 3,
+    },
+    992: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        slidesPerGroup: 1,
+    },
+},
 });
 
-function getDirection() {
-  var windowWidth = window.innerWidth;
-  var direction = window.innerWidth <= 760 ? "vertical" : "horizontal";
-
-  return direction;
-}
+// loop: true,
+// loopFillGroupWithBlank: true,
