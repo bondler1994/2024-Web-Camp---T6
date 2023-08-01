37321756911bd5ee67b7d71ca3b30740f16c0389
YenChenJulia <fairy750122gmail.com>
2023-08-01 09:16:53 +0800

    0725

--- julia/swiper.js
+++ julia/swiper.js
@@ -1,8 +1,28 @@
+
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  slidesPerView: 4,
+  slidesPerView: 'auto',
+  breakpoints: {
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 24
+    },
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 24
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    }
+  },
+  // slidesPerView: 4,
+  // spaceBetween: 20,   
   // spaceBetween:slide之間的距離
-  spaceBetween: 20, 
+   
   loop: true,  
 
   // Navigation arrows
@@ -11,3 +31,5 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   }, 
 });
+
+
